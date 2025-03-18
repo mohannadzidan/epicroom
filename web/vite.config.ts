cd1d9a93f8ef7fa63708fcb0df17f6140ec33c59
mohannadzidan <mohannad.s.zidan@gmail.com>
2025-03-18 23:26:17 +0200

    feat(web): add initial web project

---
+++ web/vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite";
+import preact from "@preact/preset-vite";
+import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    preact(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "Epic room",
+        short_name: "Epicroom",
+        description: "To control the room and automate some stuff",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
